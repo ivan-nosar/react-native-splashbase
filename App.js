2ff3fe65be02098138d33858f6aba6b610ef8b4d
Alex Margaritov <v-almarg@microsoft.com>
2018-10-02 14:20:40 +0300

    Updated app logic

--- App.js
+++ App.js
@@ -1,38 +1,56 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * @format
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button, Image} from 'react-native';
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
+  state = {
+    imageURL: null,
+    imageRequested: false,
+    isImageLoading: false
+  }
+
+  buttonClickHandler = async () => {
+    this.setState({ imageURL: null, imageRequested: true, isImageLoading: false })
+    const imageURL = await fetch('http://www.splashbase.co/api/v1/images/random')
+      .then(res => res.json())
+      .then(data => data.url)
+    this.setState({ imageURL })
+  }
+
+  imageLoadStartHandler = () => {this.setState({ isImageLoading: true })}
+  imageLoadEndHandler = () => {this.setState({ isImageLoading: false })}
+
+  render = () => {
+    const imageTemplate = this.state.imageURL ?
+      <View style={styles.imageContainer}>
+        <Text style={styles.instructions}>Current image url:</Text>
+        <Text style={styles.instructions}>{ this.state.imageURL }</Text>
+        { this.state.isImageLoading &&  <Text style={styles.instructions}>Loading image...</Text> }
+        <Image
+          style={styles.image}
+          source={{uri: this.state.imageURL }}
+          onLoadStart={this.imageLoadStartHandler}
+          onLoadEnd={this.imageLoadEndHandler}
+        />
+      </View>
+      : <Text style={styles.instructions}>Image URL is loading...</Text>;
+    
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.welcome}>Welcome to React Native Test application!</Text>
+        <Text style={styles.instructions}>Press button below to refresh image</Text>
+        
+        <Button
+          onPress={this.buttonClickHandler}
+          title="Refresh"
+        />
+
+        { this.state.imageRequested && imageTemplate }
       </View>
     );
   }
@@ -55,4 +73,13 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  imageContainer: {
+    width: '90%'
+  },
+  image: {
+    width: 200,
+    height: 200,
+    margin: 10,
+    alignSelf: 'center'
+  }
 });
